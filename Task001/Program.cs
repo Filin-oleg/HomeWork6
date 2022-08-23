db0a01fb887abfb169bae7d81b042db90222a59d
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-23 23:28:14 +0200

    done

---
+++ Task001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.WriteLine("Введите количество элеметов массива ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arrayFill(int M)
+{
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите число массива ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+int[] arr = arrayFill(M);
+Console.WriteLine($"Количество чисел больше 0 = " + Counter(arr));
