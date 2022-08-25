c90f29a2cb208ff160c9e08dd3b6751d00f2d817
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-25 14:24:40 +0200

    done

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -33,7 +33,7 @@ PrintMatrix(matrix);
 int Sums(int[,] matrix)
 {
     int Sum = 0;
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
     {
         Sum += matrix[i, i];
     }
